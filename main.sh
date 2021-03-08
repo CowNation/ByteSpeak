3f9cc76c5f3955d697605411f28fa1fad177b06c
Cow Cow <cownation.cow@gmail.com>
2021-03-08 14:26:19 +0000

    Tweaked shell

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 echo "Compiling & linking.."
-clang++-7 -pthread -std=c++17 -o main main.cpp
-./main
+clang++-7 -pthread -std=c++17 -o compiler main.cpp
+./compiler
+
 nasm -f elf output.asm
 ld -m elf_i386 -s -o build output.o
 
@@ -8,6 +9,7 @@ echo "Build complete, running.."
 ./build
 
 echo "Program has been ran, cleaning up files.."
+rm compiler
 rm output.asm
 rm output.o
 rm build
